e4b74f2aac01c3bf2b1c1c1d037f94ca5c2bfaef
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-15 23:56:30 +0200

    win condition checker is on the way

--- TicTacToe_HM/src/App.jsx
+++ TicTacToe_HM/src/App.jsx
@@ -3,6 +3,27 @@
 
 function App() {
 
+  let squares = [
+    ["x","x","x"],
+    [0,1,2],
+    [0,1,2],
+    
+  ];
+
+  
+
+  let selectedSquares = [squares[0][0],squares[0][1],squares[0][2]];
+
+  console.log(squares);
+
+  function winConditonChecker(lastSelectedSquare,squaresToWin){
+
+
+    function horizontalChecker(){
+
+    }
+
+  }
 
   return (
     <div>app</div>
@@ -10,3 +31,4 @@ function App() {
 }
 
 export default App
+ 
