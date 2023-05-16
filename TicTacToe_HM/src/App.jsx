545266c6ec9705bf00b650357d6acbfd90be6512
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-16 14:45:07 +0200

    winConditionChecker inProgresss

--- TicTacToe_HM/src/App.jsx
+++ TicTacToe_HM/src/App.jsx
@@ -4,7 +4,7 @@
 function App() {
 
   let squares = [
-    ["x","x","x"],
+    ["x","x","x","x"],
     [0,1,2],
     [0,1,2],
     
@@ -16,14 +16,39 @@ function App() {
 
   console.log(squares);
 
-  function winConditonChecker(lastSelectedSquare,squaresToWin){
+  let lastCS = [0,2];
 
+
+  function winConditonChecker(lastSelectedSquare,squaresToWin,symbol){
+
+
+    let symbolCounter = 0;
 
     function horizontalChecker(){
-
+      for(let i = lastSelectedSquare[1]; i < lastSelectedSquare[1] + squaresToWin; i++){
+        console.log("ran right" + " " + i);
+        if(squares[lastSelectedSquare[0]][i] && squares[lastSelectedSquare[0]][i] === symbol){
+          symbolCounter++;
+        }else{
+          console.log("not included");
+        }
+      }
+      for(let i = lastSelectedSquare[ 1 - 1 ]; i > lastSelectedSquare[1] - squaresToWin; i--){
+        console.log("ran left" + " " + i);
+        if(squares[lastSelectedSquare[0]][i] && squares[lastSelectedSquare[0]][i] === symbol){
+          symbolCounter++;
+        }else{
+          console.log("not included");
+        }
+      }
+      console.log(symbolCounter);
     }
 
+    horizontalChecker();
+
   }
+
+  winConditonChecker(lastCS,3,"x");
 
   return (
     <div>app</div>
